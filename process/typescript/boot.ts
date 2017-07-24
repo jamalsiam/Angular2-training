6e45366bf082ebd2aa3ac7316491f6b8b8e03620
jamalsiam <jamalsiam1995@gmail.com>
2017-07-24 12:23:35 +0300

    first commit

---
+++ process/typescript/boot.ts
@@ -0,0 +1,6 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+import {AppInfo} from './info.component';
+
+bootstrap(AppComponent);
+bootstrap(AppInfo);
